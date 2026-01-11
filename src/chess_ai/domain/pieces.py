0113900f7fb608706c8a9034b6f1a39914284a9f
basti <malte.hartmann@tu-dortmund.de>
2026-01-11 09:02:13 +0100

    add bishop movement logic

--- src/chess_ai/domain/pieces.py
+++ src/chess_ai/domain/pieces.py
@@ -230,6 +230,8 @@ class Knight(Piece):  # Springer
 
     def get_reachable_cells(self):
         """
+        **TODO** Implement the movability mechanic for `knights <https://de.wikipedia.org/wiki/Springer_(Schach)>`_.
+
         **NOTE**: Here you do not yet need to consider whether your own King would become checked after a move. This will be taken care of by
         the :py:meth:`is_king_check <board.Board.is_king_check>` and :py:meth:`get_valid_cells <pieces.Piece.get_valid_cells>` methods.
 
@@ -242,30 +244,8 @@ class Knight(Piece):  # Springer
 
         :return: A list of reachable cells this knight could move into.
         """
-        reachable_cells = []
-        row, col = self.cell
+        # TODO: Implement a method that returns all cells this piece can enter in its next move
 
-        directions = {
-            (-2, -1),
-            (-2, 1),
-            (2, -1),
-            (2, 1),
-            (-1, -2),
-            (-1, 2),
-            (1, -2),
-            (1, 2),
-        }
-
-        for direction_row, direction_col in directions:
-            target_cell = (row + direction_row, col + direction_col)
-
-            if self.board.cell_is_valid_and_empty(target_cell):
-                reachable_cells.append(target_cell)
-
-            elif self.board.piece_can_hit_on_cell(target_cell):
-                reachable_cells.append(target_cell)
-
-        return reachable_cells
 
 class Bishop(Piece):  # Läufer
     def __init__(self, board, white):
@@ -273,8 +253,6 @@ class Bishop(Piece):  # Läufer
 
     def get_reachable_cells(self):
         """
-        **TODO** Implement the movability mechanic for `bishop <https://de.wikipedia.org/wiki/L%C3%A4ufer_(Schach)>`_.
-
         **NOTE**: Here you do not yet need to consider whether your own King would become checked after a move. This will be taken care of by
         the :py:meth:`is_king_check <board.Board.is_king_check>` and :py:meth:`get_valid_cells <pieces.Piece.get_valid_cells>` methods.
 
@@ -286,7 +264,37 @@ class Bishop(Piece):  # Läufer
 
         :return: A list of reachable cells this bishop could move into.
         """
-        # TODO: Implement a method that returns all cells this piece can enter in its next move
+        reachable_cells = []
+
+        row, col = self.cell
+
+        directions = {
+            (-1, -1),
+            (1, -1),
+            (-1, -1),
+            (1, 1)
+        }
+
+        for direction_row, direction_col in directions:
+            current_row = row + direction_row
+            current_col = col + direction_col
+
+            while True:
+                current_cell = (current_row, current_col)
+
+                if self.board.cell_is_valid_and_empty(current_cell):
+                    reachable_cells.append(current_cell)
+
+                    current_row += direction_row
+                    current_col += direction_col
+
+                elif self.can_hit_on_cell(current_cell):
+                    reachable_cells.append(current_cell)
+                    break
+                else:
+                    break
+
+        return reachable_cells
 
 
 class Queen(Piece):  # Königin
